a67dadbf349e8bb5f51a9a302c726c2547452c1b
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2023-01-22 16:19:53 +0100

    refactored list of books

--- www/books.js
+++ www/books.js
@@ -1,11 +1,15 @@
 import { books } from './global.js';
 
 const list = document.getElementById('books');
-console.log(list);
 
-books.forEach(({ title, author }) => {
-  const book = document.createElement('div');
-  book.classList = ['item'];
-  book.innerHTML = `<h4 class="item-title">${title}</h4><p class="item-para">${author}</p>`;
-  list.appendChild(book);
-});
+Array.isArray(books) &&
+  books.forEach(({ title, author }) => {
+    const _title = document.createElement('h4');
+    _title.className += 'item item-title';
+    _title.innerHTML = title;
+    list.appendChild(_title);
+    const _author = document.createElement('p');
+    _author.className = 'item item-para';
+    _author.innerHTML = 'by ' + author;
+    list.appendChild(_author);
+  });
