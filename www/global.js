3b55041e789fa13cbf745b17629f6b181da41f37
Arsnumeralis <arsnumeralis@gmail.com>
2023-02-02 18:22:55 +0000

    adding set and read from local storage

--- www/global.js
+++ www/global.js
@@ -1,10 +1,38 @@
 const headers = new Headers();
 headers.append('content-type', 'application/json');
 
-const result = await fetch('http://localhost:3009/graphql', {
-  method: 'POST',
-  headers,
-  body: JSON.stringify({ query: '{books{title,author}}' })
-}).then(response => response.json());
 
-export const books = result?.data?.books;
+
+export async function setLocalStorage(key, value) {
+  // expiry check to be added
+  const now = new Date();
+  const item = await {
+    value: value,
+    // expiry: now.getTime() + expiry
+  }
+  await localStorage.setItem(key, JSON.stringify(item));
+}
+
+let booksObj
+
+
+export async function getBooks(key) {
+  // expiry check to be added
+  const itemStr = await localStorage.getItem(key);
+  if (!itemStr) {
+    const result = await fetch('http://localhost:3000/graphql', {
+      method: 'POST',
+      headers,
+      body: JSON.stringify({ query: '{books{title,author}}' })
+    }).then(response => response.json());
+    booksObj = result.data.books
+    console.log(booksObj)
+    console.log(await localStorage.getItem("books"))
+    setLocalStorage("books", books, expiry)
+    return booksObj} else {
+      booksObj = await JSON.parse(window.localStorage.getItem('books'));
+      return booksObj.value
+    }
+}
+
+export const books = await getBooks("books");
