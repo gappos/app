4c9a3cd3f4b787a706b73ceabdbe9b2bf3430eee
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2023-06-12 18:43:39 +0200

    installed redux

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo', 'module:metro-react-native-babel-preset'],
-    entryFile: './src/App.tsx',
+    presets: ['babel-preset-expo'],
   };
 };
