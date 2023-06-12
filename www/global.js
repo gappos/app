aa7a207bc70489cc6dee5ef434fa68a5029f409e
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2023-06-12 12:43:32 +0200

    initial commit

--- www/global.js
+++
@@ -1,7 +0,0 @@
-const headers = new Headers();
-headers.append("content-type", "application/json");
-
-import BooksState from "./states.js";
-const booksState = new BooksState();
-
-export const books = await booksState.getBooks("books");
