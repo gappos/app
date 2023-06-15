2fca24d294d55a9fbc05bbac1f95a3c866b42f04
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2023-06-15 17:46:20 +0200

    added Button component

---
+++ components/Button.tsx
@@ -0,0 +1,47 @@
+import {
+  Pressable,
+  Text,
+  TextStyle,
+  TouchableOpacityProps,
+} from 'react-native';
+import { useSelector } from 'react-redux';
+import { State } from '../redux';
+import { colors } from '../config';
+
+interface Props extends TouchableOpacityProps {
+  title?: string;
+  titleStyle?: TextStyle;
+}
+
+const Button = (props: Props) => {
+  const theme = useSelector((state: State) => state.theme.theme);
+
+  const {
+    title = props.children ? props.children : 'Enter',
+    titleStyle,
+    style,
+    ...restProps
+  } = props;
+  const backgroundColor = colors[theme].buttonColor;
+  const color = colors[theme].color;
+  return (
+    <Pressable
+      style={{ backgroundColor, padding: 6, ...(style as Object) }}
+      {...restProps}
+    >
+      <Text
+        style={{
+          color,
+          textTransform: 'uppercase',
+          alignSelf: 'center',
+          fontWeight: 'bold',
+          ...titleStyle,
+        }}
+      >
+        {title}
+      </Text>
+    </Pressable>
+  );
+};
+
+export default Button;
