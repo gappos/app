7c7b9235a2f8cf061bcb6c0caf17ee5d39a04c60
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2023-06-15 17:46:46 +0200

    moved stuff to components

---
+++ components/ScreenInfo.tsx
@@ -0,0 +1,31 @@
+import { View, Text, Dimensions, StyleSheet } from 'react-native';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
+import { useSelector } from 'react-redux';
+
+import { State } from '../redux';
+import { colors } from '../config';
+
+export default function ScreenInfo() {
+  const theme = useSelector((state: State) => state.theme.theme);
+
+  const styles = StyleSheet.create({
+    para: {
+      color: colors[theme].color,
+      fontSize: 20,
+      fontWeight: '400',
+    },
+  });
+
+  return (
+    <View>
+      <Text style={styles.para}>{`screen is ${JSON.stringify(
+        Dimensions.get('screen'),
+        null,
+        2,
+      )}`}</Text>
+      <Text
+        style={styles.para}
+      >{`Status bar height is ${getStatusBarHeight()}`}</Text>
+    </View>
+  );
+}
