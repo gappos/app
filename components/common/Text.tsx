d4b0e42364c9c51f64af73893f3a513e3ee5debc
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2023-06-17 12:21:14 +0200

    updated Text

--- components/common/Text.tsx
+++ components/common/Text.tsx
@@ -1,4 +1,4 @@
-import { ColorValue, Text, TextProps } from 'react-native';
+import { Text, TextProps } from 'react-native';
 import { useSelector } from 'react-redux';
 
 import { State } from '../../redux';
@@ -6,10 +6,9 @@ import { colors } from '../../config';
 
 const _Text = ({ children, style, ...remainingProps }: TextProps) => {
   const theme = useSelector((state: State) => state.theme.theme);
-  const { color, ...remainingStyleProps } = style as { color: ColorValue };
   return (
     <Text
-      style={{ color: colors[theme].color, ...remainingStyleProps }}
+      style={{ color: colors[theme].color, ...(style as {}) }}
       {...remainingProps}
     >
       {children}
