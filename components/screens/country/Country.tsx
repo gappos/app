32ac64f61f7fcc698b1a844194f29dab510c7150
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2023-06-19 21:40:42 +0200

    expand/collapse country, cities, places

--- components/screens/country/Country.tsx
+++ components/screens/country/Country.tsx
@@ -1,4 +1,5 @@
-import { StyleSheet, View, ViewProps } from 'react-native';
+import { Pressable, StyleSheet, View, ViewProps } from 'react-native';
+import { useState } from 'react';
 
 import { Country as CountryType } from '../../../graphql';
 import Text from '../../common/Text';
@@ -9,37 +10,54 @@ interface Props extends ViewProps {
 }
 
 const styles = StyleSheet.create({
+  country: {},
   locations: { marginLeft: 20 },
 });
 
-const Country = ({ country, ...props }: Props) => (
-  <View {...props}>
-    <Text>{country.country}</Text>
-    {country?.cities.length ? (
-      <View style={styles.locations}>
-        <Text>Cities:</Text>
-        {country.cities.map(city => (
-          <Location
-            key={city.city}
-            name={city.city}
-            style={{ marginLeft: 30 }}
-          />
-        ))}
-      </View>
-    ) : null}
-    {country?.places.length ? (
-      <View style={styles.locations}>
-        <Text>Places:</Text>
-        {country.places.map(place => (
-          <Location
-            key={place.place}
-            name={place.place}
-            style={{ marginLeft: 30 }}
-          />
-        ))}
-      </View>
-    ) : null}
-  </View>
-);
+const Country = ({ country, style, ...remainingProps }: Props) => {
+  const [show, setShow] = useState(false);
+  const [showCities, setShowCities] = useState(false);
+  const [showPlaces, setShowPlaces] = useState(false);
+
+  return (
+    <View style={{ ...styles.country, ...(style as {}) }} {...remainingProps}>
+      <Pressable onPress={() => setShow(prev => !prev)}>
+        <Text>{country.country}</Text>
+      </Pressable>
+      {show ? (
+        <View>
+          <View style={styles.locations}>
+            <Pressable onPress={() => setShowCities(prev => !prev)}>
+              <Text>Cities:</Text>
+            </Pressable>
+            {showCities && country?.cities.length
+              ? country.cities.map(city => (
+                  <Location
+                    key={city.city}
+                    name={city.city}
+                    style={{ marginLeft: 30 }}
+                  />
+                ))
+              : null}
+          </View>
+          <View style={styles.locations}>
+            <Pressable onPress={() => setShowPlaces(prev => !prev)}>
+              <Text>Places:</Text>
+            </Pressable>
+            {showPlaces && country?.places.length
+              ? country.places.map(place => (
+                  <Location
+                    key={place.place}
+                    name={place.place}
+                    style={{ marginLeft: 30 }}
+                  />
+                ))
+              : null}
+          </View>
+        </View>
+      ) : null}
+    </View>
+  );
+};
 
 export default Country;
