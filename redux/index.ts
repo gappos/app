2f704869df5bd2a26d2139221e3619e4f1e0d5af
Arturas Mickiewicz <arturasmckwcz@gmail.com>
2023-06-30 11:15:58 +0200

    added locationId slice to redux

--- redux/index.ts
+++ redux/index.ts
@@ -2,3 +2,5 @@ export * from './store';
 export * from './types';
 export * from './reducers/counter';
 export * from './reducers/theme';
+export * from './reducers/screen';
+export * from './reducers/locationId';
--- redux/store.ts
+++ redux/store.ts
@@ -2,16 +2,22 @@ import { configureStore } from '@reduxjs/toolkit';
 
 import { counter } from './reducers/counter';
 import { theme } from './reducers/theme';
-import { Theme } from './types';
+import { screen } from './reducers/screen';
+import { locationId } from './reducers/locationId';
+import { Screens, Theme } from './types';
 
 export interface State {
   theme: { theme: Theme };
   counter: { count: number };
+  screen: { screen: Screens };
+  locationId: { locationId: string };
 }
 
 export const store = configureStore({
   reducer: {
     counter,
     theme,
+    screen,
+    locationId,
   },
 });
